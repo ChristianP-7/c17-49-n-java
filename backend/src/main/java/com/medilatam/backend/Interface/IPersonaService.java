7ab21ab068ea15d4b4647fb1c6f8abfe69e81048
Esteban Camacho Salgado <esteban.camacho@jala.university>
2024-04-22 18:46:25 -0500

    feat: Cloudinary imagenes en la nube.

--- backend/src/main/java/com/medilatam/backend/Interface/IPersonaService.java
+++ backend/src/main/java/com/medilatam/backend/Interface/IPersonaService.java
@@ -1,6 +1,9 @@
 package com.medilatam.backend.Interface;
 
+import org.springframework.web.multipart.MultipartFile;
 import com.medilatam.backend.Entity.PersonaEntity;
+
+import java.io.IOException;
 import java.util.List;
 
 public interface IPersonaService {
@@ -12,12 +15,12 @@ public interface IPersonaService {
     PersonaEntity personaPorId(Long id);
 
     //Almacena un nuevo usuario
-    void guardarPersona(PersonaEntity personaEntity);
+    void guardarPersona(PersonaEntity personaEntity, MultipartFile fileIcon) throws IOException;
 
     //Edita un usuario basandose en su ID
-    void actualizarPersonaPorId(Long id, PersonaEntity personaDetails);
+    void actualizarPersonaPorId(Long id, PersonaEntity personaEntity, MultipartFile fileIcon) throws IOException;
 
     //Elimina un usuario buscandolo por su ID
-    void eliminarPersonaPorId(Long id);
+    void eliminarPersonaPorId(Long id) throws IOException;
 
 }
--- backend/src/main/java/com/medilatam/backend/Service/PersonaService.java
+++ backend/src/main/java/com/medilatam/backend/Service/PersonaService.java
@@ -4,13 +4,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.medilatam.backend.Entity.PersonaEntity;
 import com.medilatam.backend.Interface.IPersonaService;
 import com.medilatam.backend.Repository.PersonaRepository;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.io.IOException;
+import java.util.Map;
 
 @Service
 public class PersonaService implements IPersonaService {
+
+    private final Cloudinary cloudinary;
+
+    public PersonaService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -31,26 +42,47 @@ public class PersonaService implements IPersonaService {
     }
 
     @Override
-    public void guardarPersona(PersonaEntity personaEntity) {
+    public void guardarPersona(PersonaEntity personaEntity, MultipartFile fileIcon) throws IOException {
+        if (!fileIcon.isEmpty()) {
+            if (fileIcon.getContentType().startsWith("image/")) {
+                Map uploadResult = cloudinary.uploader().upload(fileIcon.getBytes(), ObjectUtils.emptyMap());
+                personaEntity.setFoto(uploadResult.get("url").toString());
+            } else {
+                throw new IllegalArgumentException("El archivo debe ser una imagen.");
+            }
+        }
         personaEntity.setPassword(passwordEncoder.encode(personaEntity.getPassword()));
         personaRepository.save(personaEntity);
     }
 
     @Override
-    public void actualizarPersonaPorId(Long id, PersonaEntity personaDetails) {
-
+    public void actualizarPersonaPorId(Long id, PersonaEntity personaData, MultipartFile fileIcon) throws IOException {
         PersonaEntity personaEntity = personaRepository.findById(id).orElse(null);
-        //Name deprecate?
-        personaEntity.setName(personaDetails.getName());
-        personaEntity.setEmail(personaDetails.getEmail());
+        personaEntity.setName(personaData.getName());
+        personaEntity.setEmail(personaData.getEmail());
         personaEntity.setPassword(passwordEncoder.encode(personaEntity.getPassword()));
-        personaEntity.setFoto(personaDetails.getFoto());
-
+        if (personaEntity.getFoto() != null) {
+            String publicId = personaEntity.getFoto().substring(personaEntity.getFoto().lastIndexOf("/") + 1, personaEntity.getFoto().lastIndexOf("."));
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        }
+        if (!fileIcon.isEmpty()) {
+            if (fileIcon.getContentType().startsWith("image/")) {
+                Map uploadResult = cloudinary.uploader().upload(fileIcon.getBytes(), ObjectUtils.emptyMap());
+                personaEntity.setFoto(uploadResult.get("url").toString());
+            } else {
+                throw new IllegalArgumentException("El archivo debe ser una imagen.");
+            }
+        }
         personaRepository.save(personaEntity);
     }
 
     @Override
-    public void eliminarPersonaPorId(Long id) {
+    public void eliminarPersonaPorId(Long id) throws IOException {
+        PersonaEntity personaEntity = personaRepository.findById(id).orElse(null);
+        if (personaEntity.getFoto() != null) {
+            String publicId = personaEntity.getFoto().substring(personaEntity.getFoto().lastIndexOf("/") + 1, personaEntity.getFoto().lastIndexOf("."));
+            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+        }
         personaRepository.deleteById(id);
     }
 
